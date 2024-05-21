acd2d8bea367844360df73b73d8d693cf22a9c6d
Blaine Freestone <blainefreestone01@gmail.com>
2024-05-21 09:57:13 -0600

    added basic arguments for script to work

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import argparse
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        prog="pdf-parser",
+        description="Uses OpenAI gpt-4-turbo to describe the contents of each page of a PDF file.",
+    )
 
+    parser.add_argument('pdf-filepath', type=str, help='Path to the PDF file to be parsed.')
+    
+    parser.add_argument('output-filepath', type=str, help='Path to the file where the output will be written.')
+
+    parser.add_argument('--system-instructions', '-s', default='system-instructions.txt', help='Path to the file containing system instructions.')
+
+    args = parser.parse_args()
